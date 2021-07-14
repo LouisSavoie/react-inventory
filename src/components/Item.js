e0824096483cb6f6d5089f77ca304a3e41d9ebb4
Louis Savoie <thelousavoie@gmail.com>
2021-07-14 16:10:13 -0400

    format Items and Item as a table

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,12 @@
 const Item = ({ item, onIncrement, onDecrement, onDelete }) => {
   return (
-    <div>
-      <p>{item.name}: {item.qty}</p>
-      <button onClick={() => onDecrement(item.id)}>-</button>
-      <button onClick={() => onIncrement(item.id)}>+</button>
-      <button onClick={() => onDelete(item.id)}>X</button>
-    </div>
+    <tr>
+      <td>{item.name}</td>
+      <td><button onClick={() => onIncrement(item.id)}>+</button></td>
+      <td>{item.qty}</td>
+      <td><button onClick={() => onDecrement(item.id)}>-</button></td>
+      <td><button onClick={() => onDelete(item.id)}>X</button></td>
+    </tr>
   );
 }
 
--- src/components/Items.js
+++ src/components/Items.js
@@ -2,14 +2,25 @@ import Item from './Item'
 
 const Items = ({ items, onIncrement, onDecrement, onDelete }) => {
   return (
-    <>
-      {items.map((item, id) => (
-        <Item key={id} item={item} 
-          onIncrement={onIncrement} 
-          onDecrement={onDecrement} 
-          onDelete={onDelete} />
-      ))}
-    </>
+    <table>
+      <thead>
+        <tr>
+          <th>Item</th>
+          <th>In</th>
+          <th>Qty</th>
+          <th>Out</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((item, id) => (
+          <Item key={id} item={item} 
+            onIncrement={onIncrement} 
+            onDecrement={onDecrement} 
+            onDelete={onDelete} />
+        ))}
+      </tbody>
+    </table>
   );
 }
 
